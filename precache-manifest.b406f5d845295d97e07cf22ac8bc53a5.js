66b5a8490a71ad29601b758cf5b2814c810db6b0
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2020-06-22 11:06:42 +0300

    Updates

---
+++ precache-manifest.b406f5d845295d97e07cf22ac8bc53a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5287231ae4d6e6dfd056d19c25c91d68",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6ff912068d5f536fe3c2",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0c3650cd610770226025",
+    "url": "/robofriends/static/css/main.bf937e89.chunk.css"
+  },
+  {
+    "revision": "6ff912068d5f536fe3c2",
+    "url": "/robofriends/static/js/2.736fa81c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.736fa81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3650cd610770226025",
+    "url": "/robofriends/static/js/main.2759aed0.chunk.js"
+  },
+  {
+    "revision": "e2551f67d460b1521312",
+    "url": "/robofriends/static/js/runtime-main.beb10e27.js"
+  }
+]);
